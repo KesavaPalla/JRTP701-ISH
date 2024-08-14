7931d233e777f45817775125e4bd2dfb23e46e3c
kesava <kesava713@gmail.com>
2024-08-14 10:48:44 +0530

    commit

--- NTRP701-ApplicationRegistrationService/src/main/java/com/nt/service/CitizenAppRegistrationServiceImpl.java
+++ NTRP701-ApplicationRegistrationService/src/main/java/com/nt/service/CitizenAppRegistrationServiceImpl.java
@@ -3,16 +3,16 @@ package com.nt.service;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.nt.bindings.CitizenAppRegistrationInputs;
 import com.nt.entity.CitizenAppRegistrationEntity;
 import com.nt.exceptions.InvalidSSNException;
 import com.nt.repository.IApplicationRegistrationRepository;
+
+import reactor.core.publisher.Mono;
 
 @Service
 public class CitizenAppRegistrationServiceImpl implements ICitizenAppRegistrationService {
@@ -37,7 +37,10 @@ public class CitizenAppRegistrationServiceImpl implements ICitizenAppRegistratio
 		*/
 		//perform  web service call  to check weather  SSN is valid or not  and get them state name. Using WebClient
 		//get State name
-		String stateName=client.get().uri(endPointURL,inputs.getSsn()).retrieve().bodyToMono(String.class).block();
+		
+		Mono<String>response=client.get().uri(endPointURL,inputs.getSsn()).retrieve()
+				                                         .onStatus(HttpStatus.BAD_REQUEST::equals, res->res.bodyToMono(String.class).map(ex->new InvalidSSNException("Invalid SSN"))).bodyToMono(String.class);
+		String stateName=response.block();
 		//register the citizen if he belongs to California state 
 		if(stateName.equalsIgnoreCase(targetState)) {
 			//prepare the entity class
